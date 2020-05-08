c8ad9bb84f99544c6e066cf0b61816d84a4f29a2
RaviAthmakuri <ravi.athmakuri93@gmail.com>
2020-05-08 23:36:01 +0530

    first

---
+++ src/main/java/technicalblog/controller/HomeController.java
@@ -0,0 +1,50 @@
+package technicalblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import technicalblog.model.Post;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String getAllPosts(Model model) {
+
+        List<Post> postList = new ArrayList<>();
+
+        Post post = new Post();
+
+        post.setTitle("Post 1");
+        post.setBody("Post Body 1");
+        post.setDate(new Date());
+
+
+        Post post2 = new Post();
+
+        post2.setTitle("Post 2");
+        post2.setBody("Post Body 2");
+        post2.setDate(new Date());
+
+
+        Post post3 = new Post();
+
+        post3.setTitle("Post 3");
+        post3.setBody("Post Body 3");
+        post3.setDate(new Date());
+
+
+        postList.add(post);
+        postList.add(post2);
+        postList.add(post3);
+
+        model.addAttribute("posts", postList);
+
+        return "index";
+
+    }
+}
